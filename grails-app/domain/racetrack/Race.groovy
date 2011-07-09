76a0a9eae2eeb3701402e290d937e1d4804d2245
Keith Brophy <keith.brophy@gmail.com>
2011-07-09 17:59:58 +1000

    Added Race and Registration controllers

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -9,6 +9,17 @@ class Race {
 	BigDecimal cost
 	Integer maxRunners = 100000
 
+    static mapping = {
+        sort "startDate"
+    }
+
     static constraints = {
+        name(blank:false, maxSize:50)
+        startDate()
+        city()
+        state(inList:["GA", "NC", "SC", "VA"])
+        distance(min:0.0)
+        cost(min:0.0, max:100.0)
+        maxRunners(min:0, max:100000)
     }
 }
