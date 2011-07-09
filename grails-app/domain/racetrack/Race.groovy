fdb36ce34e23b1a1ea9879d1e330c3a6b7013609
Keith Brophy <keith.brophy@gmail.com>
2011-07-09 17:01:30 +1000

    Adding Race domain object

---
+++ grails-app/domain/racetrack/Race.groovy
@@ -0,0 +1,14 @@
+package racetrack
+
+class Race {
+	String name
+	Date startDate
+	String city
+	String state
+	BigDecimal distance
+	BigDecimal cost
+	Integer maxRunners = 100000
+
+    static constraints = {
+    }
+}
