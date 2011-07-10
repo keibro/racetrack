02f49ed20d02d22fd4ec0c13f9f258f798c9c100
Keith Brophy <keith.brophy@gmail.com>
2011-07-10 22:16:04 +1000

    Adding Runner bootstrap dev data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,26 @@
 class BootStrap {
 
     def init = { servletContext ->
+        println "BootStrap init ..."
+        def jane = new racetrack.Runner(
+            firstName:"Jane",
+            lastName:"Doe",
+            dateOfBirth:(new Date() - 365*30),
+            gender:"F",
+            address:"123 Main St",
+            city:"Goose",
+            state:"NC",
+            zipcode:"12345",
+            email:"jane@whereever.com"
+            )
+        jane.save()
+        if(jane.hasErrors()){
+            println jane.errors
+        }
+        else
+        {
+            println jane.lastName + " saved"
+        }
     }
     def destroy = {
     }
