7cf476ab64a1ebe81a3045c24fe11eab427e5d41
Keith Brophy <keith.brophy@gmail.com>
2011-07-14 21:14:02 +1000

    Add bootstrap information for a Registration and a Race.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,26 +1,66 @@
+import racetrack.Race
+import racetrack.Registration
+import racetrack.Runner
+import grails.util.GrailsUtil
+
 class BootStrap {
 
     def init = { servletContext ->
         println "BootStrap init ..."
-        def jane = new racetrack.Runner(
-            firstName:"Jane",
-            lastName:"Doe",
-            dateOfBirth:(new Date() - 365*30),
-            gender:"F",
-            address:"123 Main St",
-            city:"Goose",
-            state:"NC",
-            zipcode:"12345",
-            email:"jane@whereever.com"
-            )
-        jane.save()
-        if(jane.hasErrors()){
-            println jane.errors
+
+        switch (GrailsUtil.environment) {
+            case "development":
+                def jane = new Runner(
+                        firstName: "Jane",
+                        lastName: "Doe",
+                        dateOfBirth: (new Date() - 365 * 30),
+                        gender: "F",
+                        address: "123 Main St",
+                        city: "Goose",
+                        state: "NC",
+                        zipcode: "12345",
+                        email: "jane@whereever.com"
+                )
+                jane.save()
+                if (jane.hasErrors()) {
+                    println jane.errors
+                }
+                else {
+                    println jane.lastName + " saved"
+                }
+
+                def trot = new Race(
+                        name: "Turkey Trot",
+                        startDate: (new Date() + 90),
+                        city: "Duck",
+                        state: "NC",
+                        distance: 5.0,
+                        cost: 20.0,
+                        maxRunners: 350
+                )
+                trot.save()
+                if (trot.hasErrors()) {
+                    println trot.errors
+                }
+
+                def reg = new Registration(
+                        paid: false,
+                        runner: jane,
+                        race: trot
+                )
+                reg.save()
+                if (reg.hasErrors()) {
+                    println reg.errors
+                }
+
+                break
+
+            case "production": break
         }
-        else
-        {
-            println jane.lastName + " saved"
-        }
+
+        println "BootStrap completed."
+
+
     }
     def destroy = {
     }
