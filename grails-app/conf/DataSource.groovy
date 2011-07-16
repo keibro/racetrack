70e3a0910d73e306fe8a43aa6f8291f5d4d3f6d1
Keith Brophy <keith.brophy@gmail.com>
2011-07-16 21:40:21 +1000

    Migrate to use a MySQL database

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,7 +1,7 @@
 dataSource {
     pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "grooser"
     password = ""
 }
 hibernate {
@@ -14,19 +14,21 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
+//            url = "jdbc:hsqldb:mem:devDB"
+            url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
+            url = "jdbc:mysql://localhost:3306/racetrack_test?autoreconnect=true"
+
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+            url = "jdbc:mysql://localhost:3306/racetrack_prd?autoreconnect=true"
         }
     }
 }
