0a8f8a8618fe700a22ff71aa117f4831962e130c
Keith Brophy <keith.brophy@gmail.com>
2012-01-26 17:18:17 +1100

    Adding AdminController

---
+++ grails-app/controllers/racetrack/AdminController.groovy
@@ -0,0 +1,21 @@
+package racetrack
+
+class AdminController {
+
+    def beforeInterceptor = [action: this.&auth]
+
+    def auth() {
+        if (!session.user) {
+            redirect(controller: "user", action: "login")
+            return false
+        }
+        if (!session.user.admin) {
+            flash.message = "Tsk tsk—admins only"
+            redirect(controller: "race", action: "list")
+            return false
+        }
+    }
+
+    def index() { }
+
+}
